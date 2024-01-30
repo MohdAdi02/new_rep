70f6b345e36bd9a46e9e6857c3b747240aa911c9
LAPTOP-AU0SMQI8\mohd adil <md7248699370@gmail.com>
2024-01-30 16:19:54 +0530

    alphabet with their ascii value

---
+++ q2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+
+
+char ch;
+printf("alphabet with their ascii value\n");
+for(ch = 'A'; ch <='Z'; ch++){
+    printf("alphabet %c and their ascii value = %d\n ",ch, ch);
+}
+
+}
